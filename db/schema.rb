a824d3588b2649c2546a3bf0bf6ef8a176685e9e
shingo kubomura <skksb75@gmail.com>
2022-06-17 13:31:57 +0900

    create DB

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_05_10_024103) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
