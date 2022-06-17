cfed4fa77790eb2738e5b13644816f602471f175
shingo kubomura <skksb75@gmail.com>
2022-06-17 13:28:18 +0900

    set root to tasks#index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root "tasks#index"
   # Read(all)
   get "/tasks", to: "tasks#index", as: :tasks
 
